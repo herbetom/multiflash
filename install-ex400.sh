cace51cce3e4cb0ac14e77c9805bda673a795aa7
Tom Herbers <mail@tomherbers.de>
2025-05-18 14:45:17 +0200

    init

---
+++ install-ex400.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+FACTORY_FILENAME="openwrt-ramips-mt7621-genexis_pulse-ex400-factory.bin"
+#FACTORY_FILENAME="openwrt-24.10.1-df5c5ed0dff7-ramips-mt7621-genexis_pulse-ex400-factory.bin"
+
+function wait_until_as_expected {
+	TARGET_URL=$1
+	TARGET_STRING=$2
+
+	echo "Checking wheter \"${TARGET_STRING}\" is present at \"${TARGET_URL}\""
+	while true; do
+		if curl --connect-timeout 2 -fs "${TARGET_URL}" | grep "${TARGET_STRING}"; then
+			echo "found"
+			break
+		else
+			echo "missing. Will try again shortly"
+		fi
+		sleep 1
+	done
+}
+
+function uploadFactoryFileEX400 {
+	FILENAME=$1
+
+	echo "will now upload factory firmware"
+
+	curl -F "fileID=@${FILENAME}" -F "submit=upload" http://192.168.1.1/firmware_upload -v
+}
+
+function repeat_ssh() {
+	COMMAND=$1
+
+	until ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@192.168.1.1 $COMMAND; do
+		sleep 2
+	done
+}
+
+wait_until_as_expected "http://192.168.1.1/" "fileID"
+sleep 2
+
+uploadFactoryFileEX400 "${FACTORY_FILENAME}"
+#sleep 20
+
+repeat_ssh "uname -a"
+sleep 2
+
+echo "copying gluon"
+scp -O -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null gluon-ffda-3.0.6~20250515-ea0b4f1-genexis-pulse-ex400-sysupgrade.bin root@192.168.1.1:/tmp/gluon.bin
+sleep 1
+echo "installing gluon"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@192.168.1.1 "/sbin/sysupgrade -n /tmp/gluon.bin || true"
+sleep 5
+
+echo "wait until reachable again"
+repeat_ssh "gluon-info"
+
+echo "configure contact"
+repeat_ssh "uci get gluon-node-info.@owner[0] || uci add gluon-node-info owner"
+repeat_ssh "uci set gluon-node-info.@owner[0].contact=info@darmstadt.freifunk.net"
+
+echo "configure ssh"
+repeat_ssh "uci set ffda-ssh-manager.settings.enabled='1'"
+repeat_ssh "uci add_list ffda-ssh-manager.settings.groups='ffda'"
+
+echo "configuring mesh"
+repeat_ssh "uci del_list gluon.iface_wan.role='uplink'"
+repeat_ssh "uci add_list gluon.iface_wan.role='mesh'"
+repeat_ssh "uci del_list gluon.iface_wan.role='client'"
+repeat_ssh "uci add_list gluon.iface_lan.role='mesh'"
+
+repeat_ssh "uci set gluon.core.domain=ffda_da_240"
+
+repeat_ssh "uci set gluon-setup-mode.@setup_mode[0].enabled='0'"
+repeat_ssh "uci set gluon-setup-mode.@setup_mode[0].configured='1'"
+
+repeat_ssh "uci commit"
+repeat_ssh "gluon-reconfigure"
+
+repeat_ssh "reboot"
+
---
+++ multiflash-setup.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+if ! command -v nft 2>&1 > /dev/null
+then
+	echo "nft could not be found but is needed"
+	exit 1
+fi
+
+if ! command -v ip 2>&1 > /dev/null
+then
+	echo "ip could not be found but is needed"
+	exit 1
+fi
+
+if ! command -v jq 2>&1 > /dev/null
+then
+	echo "jq could not be found but is needed"
+	exit 1
+fi
+
+
+IFNAME=enp0s20f0u2
+#IFNAME=enp0s20f0u1
+IFNAME=enp0s20f0u2u1
+
+VID_START=100
+VID_INTERVAL=1
+VID_STEPS=4
+
+
+ip -json link show dev $IFNAME > /dev/null
+
+vid_list=()
+for ((i=0; i<VID_STEPS; i++)); do
+	vid=$((VID_START + i * VID_INTERVAL))
+	vid_list+=("$vid")
+done
+
+echo "${vid_list[@]}"
+
+echo "deleting old"
+for vid in "${vid_list[@]}" ; do
+	echo "handling ${vid}"
+	vlanif="${IFNAME}.${vid}"
+	nsname="setupns${vid}"
+
+	ip netns delete "${nsname}" 2>&1 > /dev/null || true
+	ip link set down "${vlanif}" 2>&1 > /dev/null || true
+	ip link delete "${vlanif}"  2>&1 > /dev/null || true
+	ip link delete "veth${vid}" 2>&1 > /dev/null || true
+done
+
+#exit
+
+for vid in "${vid_list[@]}" ; do
+	echo "handling ${vid}"
+	#vlanif="${IFNAME}.${vid}"
+	vlanif="mf-vlan.${vid}"
+	nsname="setupns${vid}"
+
+	ip link add link $IFNAME name "${vlanif}" type vlan id $vid || true
+	ip link add "veth${vid}" type veth peer name "veth${vid}-ns"
+
+	ip netns add "${nsname}"
+	ip link set dev "${vlanif}" netns "${nsname}"
+	ip link set dev "veth${vid}-ns" netns "${nsname}"
+
+	ip netns exec "${nsname}" ip addr add 192.168.1.10/24 dev "${vlanif}"
+	ip netns exec "${nsname}" ip link set "${vlanif}" up
+	ip netns exec "${nsname}" ip link set lo up
+
+	#ip netns exec "${nsname}" ip addr add 192.168.1.10/24 dev "veth${vid}-ns"
+	ip netns exec "${nsname}" ip addr add fe80::a/64 dev "veth${vid}-ns"
+
+	ip netns exec "${nsname}" ip link set "veth${vid}-ns" up
+
+	#ip addr add fe80::a/64 dev "veth${vid}"
+	ip link set "veth${vid}" up
+
+	ip route add 10.168.$vid.0/24 via inet6 fe80::a dev "veth${vid}"
+
+
+	ip netns exec "${nsname}" nft add table nat
+	ip netns exec "${nsname}" nft 'add chain nat postrouting { type nat hook postrouting priority 100 ; }'
+	ip netns exec "${nsname}" nft 'add chain nat prerouting { type nat hook prerouting priority -100 ; }'
+
+	ip netns exec "${nsname}" nft add rule nat postrouting counter
+	ip netns exec "${nsname}" nft add rule nat prerouting counter
+	#ip netns exec "${nsname}" nft add rule nat postrouting ip daddr 10.168.$vid.0/24 counter snat ip prefix to ip saddr map { 10.168.$vid.0/24 : 192.168.1.0/24 }
+	#ip netns exec "${nsname}" nft add rule nat prerouting ip daddr 10.168.$vid.0/24 counter dnat ip prefix to ip saddr map { 10.168.$vid.0/24 : 192.168.1.0/24 }
+
+
+	#ip netns exec "${nsname}" nft add rule nat prerouting ip daddr 10.168.$vid.0/24 counter ip daddr map { type ipv4_addr : ipv4_addr elements = { 10.168.$vid.0/24 : 192.168.1.0/24 } } dnat to ip daddr map
+	ip netns exec "${nsname}" nft add rule nat prerouting ip daddr 10.168.$vid.1 counter dnat to 192.168.1.1
+
+	ip netns exec "${nsname}" nft add rule nat postrouting oifname "${vlanif}" counter masquerade
+
+done
+
+
+
+#ip link add link eth0 name eth0.100 type vlan id 100
