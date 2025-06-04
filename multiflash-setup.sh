29128b0bb834149e0d5259617c1b27748b75901c
Tom Herbers <mail@tomherbers.de>
2025-06-05 00:53:42 +0200

    multiflash-setup.sh: cleanup

--- multiflash-setup.sh
+++ multiflash-setup.sh
@@ -30,15 +30,15 @@ then
 	exit 1
 fi
 
+# CONFIG Section Start
 
-IFNAME=enp0s20f0u2
-#IFNAME=enp0s20f0u1
-IFNAME=enp0s20f0u2u1
+IFNAME=enp1s0
 
 VID_START=100
 VID_INTERVAL=1
 VID_STEPS=4
 
+# CONFIG Section End
 
 ip -json link show dev $IFNAME > /dev/null
 
@@ -59,11 +59,9 @@ for vid in "${vid_list[@]}" ; do
 	ip netns delete "${nsname}" 2>&1 > /dev/null || true
 	ip link set down "${vlanif}" 2>&1 > /dev/null || true
 	ip link delete "${vlanif}"  2>&1 > /dev/null || true
-	ip link delete "veth${vid}" 2>&1 > /dev/null || true
 done
 
-#exit
-
+echo "creating new"
 for vid in "${vid_list[@]}" ; do
 	echo "handling ${vid}"
 	#vlanif="${IFNAME}.${vid}"
@@ -71,44 +69,11 @@ for vid in "${vid_list[@]}" ; do
 	nsname="setupns${vid}"
 
 	ip link add link $IFNAME name "${vlanif}" type vlan id $vid || true
-	ip link add "veth${vid}" type veth peer name "veth${vid}-ns"
 
 	ip netns add "${nsname}"
 	ip link set dev "${vlanif}" netns "${nsname}"
-	ip link set dev "veth${vid}-ns" netns "${nsname}"
 
 	ip netns exec "${nsname}" ip addr add 192.168.1.10/24 dev "${vlanif}"
 	ip netns exec "${nsname}" ip link set "${vlanif}" up
 	ip netns exec "${nsname}" ip link set lo up
-
-	#ip netns exec "${nsname}" ip addr add 192.168.1.10/24 dev "veth${vid}-ns"
-	ip netns exec "${nsname}" ip addr add fe80::a/64 dev "veth${vid}-ns"
-
-	ip netns exec "${nsname}" ip link set "veth${vid}-ns" up
-
-	#ip addr add fe80::a/64 dev "veth${vid}"
-	ip link set "veth${vid}" up
-
-	ip route add 10.168.$vid.0/24 via inet6 fe80::a dev "veth${vid}"
-
-
-	ip netns exec "${nsname}" nft add table nat
-	ip netns exec "${nsname}" nft 'add chain nat postrouting { type nat hook postrouting priority 100 ; }'
-	ip netns exec "${nsname}" nft 'add chain nat prerouting { type nat hook prerouting priority -100 ; }'
-
-	ip netns exec "${nsname}" nft add rule nat postrouting counter
-	ip netns exec "${nsname}" nft add rule nat prerouting counter
-	#ip netns exec "${nsname}" nft add rule nat postrouting ip daddr 10.168.$vid.0/24 counter snat ip prefix to ip saddr map { 10.168.$vid.0/24 : 192.168.1.0/24 }
-	#ip netns exec "${nsname}" nft add rule nat prerouting ip daddr 10.168.$vid.0/24 counter dnat ip prefix to ip saddr map { 10.168.$vid.0/24 : 192.168.1.0/24 }
-
-
-	#ip netns exec "${nsname}" nft add rule nat prerouting ip daddr 10.168.$vid.0/24 counter ip daddr map { type ipv4_addr : ipv4_addr elements = { 10.168.$vid.0/24 : 192.168.1.0/24 } } dnat to ip daddr map
-	ip netns exec "${nsname}" nft add rule nat prerouting ip daddr 10.168.$vid.1 counter dnat to 192.168.1.1
-
-	ip netns exec "${nsname}" nft add rule nat postrouting oifname "${vlanif}" counter masquerade
-
 done
-
-
-
-#ip link add link eth0 name eth0.100 type vlan id 100
